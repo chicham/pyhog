3f162e7cb65d0a887ec71e97d4d8d77509428969
Hicham <hicham@randrianarivo.net>
2015-01-28 15:12:21 +0100

    transform into module

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages, Extension
+import numpy as np
+
+setup(name='pyhog',
+      version='0.1a0',
+      author='Daniel Maturana',
+      packages=find_packages(),
+      ext_modules=[Extension('pyhog.features_pedro_py',
+                             extra_compile_args=['-O2', '-ffast-math',
+                                                 '-msse2', '-DNUMPYCHECK',
+                                                 '-DNDEBUG', '-fPIC'],
+                             sources=['./pyhog/features_pedro_py.cc'])],
+      include_dirs=[np.get_include()],
+      scripts=['./pyhog_example.ipynb'])
+
